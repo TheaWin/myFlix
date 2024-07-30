6cd88c74a0ea1244cc452aab3e9032742715d625
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-07-30 12:24:28 +0400

    Remove passport.authenticate
    temporarily removed passport.authenticate from /anime endpoint for testing

--- index.js
+++ index.js
@@ -229,7 +229,8 @@ app.get('/', (req, res) => {
 });
 
 //Return a list of ALL anime to the user
-app.get('/anime', passport.authenticate('jwt', {session: false}), async (req, res) => {
+//, passport.authenticate('jwt', {session: false}) [temporarily removed]
+app.get('/anime', async (req, res) => {
     await Anime.find()
     .then ((anime) => {
         res.status(201).json(anime);
