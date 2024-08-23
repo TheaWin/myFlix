2033a328529cb40b03541a171206993bf7b46d9c
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-23 18:33:41 +0400

    Update CORS

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const cors = require('cors');
 // app.use(cors());
 
 //allow only specified origins to be given access
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com','http://localhost:1234','https://anime-eiga.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com','http://localhost:1234','https://anime-eiga.netlify.app','https://theawin.github.io'];
 app.use (cors({
     origin: (origin,callback) => {
         if(!origin) return callback (null,true);
